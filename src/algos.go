bef78c7606247c246864c5c7bec0b9ab3cc75d0b
laurihak <lauri_hakala@hotmail.com>
2022-02-09 20:46:47 +0200

    running sum

--- src/algos.go
+++ src/algos.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	// fmt.Print(buildArray([]int{0, 1, 2, 3, 4, 5}))
-	fmt.Print(getConcatenation([]int{0, 1, 2, 3, 4, 5}))
+	// fmt.Print(getConcatenation([]int{0, 1, 2, 3, 4, 5}))
+	fmt.Print(runningSum([]int{0, 1, 1, 1, 1, 1}))
 }
 
 func buildArray(nums []int) []int {
@@ -25,6 +26,20 @@ func getConcatenation(nums []int) []int {
 	for i := 0; i < sizeOfArray; i++ {
 		slice[i] = nums[i]
 		slice[i+sizeOfArray] = nums[i]
+	}
+	return slice
+}
+
+func runningSum(nums []int) []int {
+	var sizeOfArray = len(nums)
+	var slice = make([]int, sizeOfArray)
+
+	for i := 0; i < sizeOfArray; i++ {
+		if i < 1 {
+			slice[i] = nums[i]
+			continue
+		}
+		slice[i] = nums[i] + slice[i-1]
 	}
 	return slice
 }
