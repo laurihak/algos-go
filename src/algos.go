dca95a650464f12eec4b8c36e1fbbdef09d55c47
laurihak <lauri_hakala@hotmail.com>
2022-02-09 20:43:02 +0200

    Concatenation of Array

--- src/algos.go
+++ src/algos.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print(buildArray([]int{0, 1, 2, 3, 4, 5}))
-
+	// fmt.Print(buildArray([]int{0, 1, 2, 3, 4, 5}))
+	fmt.Print(getConcatenation([]int{0, 1, 2, 3, 4, 5}))
 }
 
 func buildArray(nums []int) []int {
@@ -14,6 +14,17 @@ func buildArray(nums []int) []int {
 
 	for i := 0; i < sizeOfArray; i++ {
 		slice[i] = nums[nums[i]]
+	}
+	return slice
+}
+
+func getConcatenation(nums []int) []int {
+	var sizeOfArray = len(nums)
+	var slice = make([]int, sizeOfArray*2)
+
+	for i := 0; i < sizeOfArray; i++ {
+		slice[i] = nums[i]
+		slice[i+sizeOfArray] = nums[i]
 	}
 	return slice
 }
