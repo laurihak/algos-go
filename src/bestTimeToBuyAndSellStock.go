f12f666a73beaf95369339938e7bbaf1fff5471e
laurihak <lauri_hakala@hotmail.com>
2022-02-24 20:50:02 +0200

    Best Time to Buy and Sell Stock

---
+++ src/bestTimeToBuyAndSellStock.go
@@ -0,0 +1,23 @@
+package main
+
+const MaxInt = int(^uint(0) >> 1)
+
+func maxProfit(prices []int) int {
+	var lowestPrice int = MaxInt
+	var maxProfit int = 0
+	var profitNow int = 0
+
+	for _, priceNow := range prices {
+		if priceNow < lowestPrice {
+			lowestPrice = priceNow
+		}
+
+		profitNow = priceNow - lowestPrice
+
+		if maxProfit < profitNow {
+			maxProfit = profitNow
+		}
+
+	}
+	return maxProfit
+}
